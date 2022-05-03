3e59e1af3f083720d48c803c5c771271ae7fc0fd
Maciej Reimann <maciej.reimann@gmail.com>
2022-05-03 14:32:50 +0200

    js: autoboxing

---
+++ apps/playground-react/src/app/js/autoboxing.js
@@ -0,0 +1,9 @@
+// primitives don't have prototypes / methods on them
+const john = "John"
+const age = 7
+
+// whenever we try to access a method or property,
+// it's being wrapped in an object: 
+// String.prototype => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+// Number.prototype => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
+
